8f8ce161a289bddb3f24c8690c9f8d92d7789ce7
zhipengl <zhipengs.liu@intel.com>
2019-02-27 23:24:36 +0800

    Implement Pci Interrupt Affinity Agent
    Create an agent which runs on each worker node to do pci interrupt
    affinity work.
    nova-sriov installed by this new package instead of old nova-utils
    
    Below test done and pass, see detailed test spec in story link
    1) deployment test
    2) Periodic audit pci irq affinity
    3) Remove VM without sriov pci port
    4) Remove VM with sriov pci port
    5) Add VM without sriov pci port
    6) Add VM with sriov pci port
    7) Add VM without pci_irq_affinity_mask
    8) Add VM without cpu policy set
    9) VM resize test
    10) Remove one pci port for VM
    
    Code framework is like below
    +------------+        +--------------+        +------------+
    |            |        |              |        |            |
    |            |        |              |        |            |
    |  Agent.py  | -----> | affinity.py  | -----> | driver.py  |
    |            |        |              |        |            |
    |   Daemon   |        |    Conduct   |        |   Drv      |
    |            |        |              |        |            |
    +------------+        +--------------+        +------------+
    
    Story: 2004600
    Task: 28850
    Depends-on: https://review.openstack.org/#/c/640263/
    
    Change-Id: Ie668036efe4d0013fed8cd45805f0321692c76f0
    Signed-off-by: zhipengl <zhipengs.liu@intel.com>

---
+++ utilities/pci-irq-affinity-agent/pci_irq_affinity/pci_irq_affinity/config.py
@@ -0,0 +1,48 @@
+#
+# Copyright (c) 2019 StarlingX.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# All Rights Reserved.
+#
+
+""" Define configuration info for pci-irq-affinity-agent"""
+
+from six.moves import configparser
+from oslo_config import cfg
+
+pci_irq_affinity_opts = [
+    cfg.IntOpt('pci_affine_interval',
+               default=60,
+               help='Number of seconds between pci affinity updates'),
+    cfg.IntOpt('msi_irq_timeout',
+               default=45,
+               help='Number of seconds to wait for msi irq configuration'),
+    cfg.IntOpt('msi_irq_since',
+               default=6,
+               help='Number of seconds to wait for msi irqs to stabilize.'),
+    cfg.IntOpt('msi_irq_check_interval',
+               default=2,
+               help='Check interval in seconds for msi irqs to stabilize.'),
+    cfg.StrOpt('config_file',
+               default='/etc/sysinv/sysinv.conf',
+               help='Get rabbitmq config info from specific config file.'),
+    cfg.StrOpt('auth_url',
+               default='http://keystone.openstack.svc.cluster.local/v3',
+               help='get auth_url in container.'),
+]
+
+CONF = cfg.CONF
+
+
+def register_opts(conf):
+    conf.register_opts(pci_irq_affinity_opts)
+
+
+register_opts(CONF)
+
+sysconfig = configparser.ConfigParser()
+sysconfig.read(CONF.config_file)
