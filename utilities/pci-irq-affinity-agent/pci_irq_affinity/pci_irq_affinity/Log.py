3001fd9bbfb4b4d5697ff0efd928fb81b813fe99
zhipengl <zhipengs.liu@intel.com>
2019-02-27 23:24:36 +0800

    Implement Pci Interrupt Affinity Agent
    Create an agent which runs on each worker node to do pci interrupt
    affinity work.
    nova-sriov installed by this new package instead of old nova-utils
    
    Below test done and pass, see detailed test spec in story link
    1) deployment test
    2) Periodic audit pci irq affinity
    3) Remove VM without sriov pci port
    4) Remove VM with sriov pci port
    5) Add VM without sriov pci port
    6) Add VM with sriov pci port
    7) Add VM without pci_irq_affinity_mask
    8) Add VM without cpu policy set
    9) VM resize test
    10) Remove one pci port for VM
    
    Code framework is like below
    +------------+        +--------------+        +------------+
    |            |        |              |        |            |
    |            |        |              |        |            |
    |  Agent.py  | -----> | affinity.py  | -----> | driver.py  |
    |            |        |              |        |            |
    |   Daemon   |        |    Conduct   |        |   Drv      |
    |            |        |              |        |            |
    +------------+        +--------------+        +------------+
    
    Story: 2004600
    Task: 28850
    Depends-on: https://review.openstack.org/#/c/640263/
    
    Change-Id: Ie668036efe4d0013fed8cd45805f0321692c76f0
    Signed-off-by: zhipengl <zhipengs.liu@intel.com>

---
+++ utilities/pci-irq-affinity-agent/pci_irq_affinity/pci_irq_affinity/Log.py
@@ -0,0 +1,34 @@
+#
+# Copyright (c) 2019 StarlingX.
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+
+# All Rights Reserved.
+#
+
+""" Define Logger class for this agent"""
+
+import logging
+from logging import handlers
+
+LOG_NAME = '/var/log/pci_irq_affinity.log'
+class Logger(object):
+    def __init__(self, filename, level=logging.INFO, when='D', backCount=3,
+            fmt='%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'):
+        self.logger = logging.getLogger(filename)
+        format_str = logging.Formatter(fmt)
+        self.logger.setLevel(level)
+        sh = logging.StreamHandler()
+        sh.setFormatter(format_str)
+        th = handlers.TimedRotatingFileHandler(filename=filename,
+                                              when=when,backupCount=backCount,encoding='utf-8')
+        th.setFormatter(format_str)
+        self.logger.addHandler(sh)
+        self.logger.addHandler(th)
+
+
+log = Logger(LOG_NAME, level=logging.INFO)
+LOG = log.logger
